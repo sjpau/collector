2b655fc58bd87300d9d18014d566f3627106f312
flat <flatdogminsk@gmail.com>
2022-02-07 21:22:56 +0300

    added presets allocation

--- rooms.c
+++ rooms.c
@@ -1,44 +1,51 @@
 #include "rooms.h"
 #include "presets.h"
 #include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
 
 /*symbol definitions*/
 #define WALL      '#'
 
-/*TODO: allow user to choose specific char symbol*/
+/*setting up drawing time interval*/
+#define TINT      15
+
+/*Prints Horizontal Wall*/
 void PrintHorizontal(int width, int stX, int stY, int edge, bool right) {
   if(right) {
     for (int i = 0; i < width + edge; ++i) {
-      mvaddch(stY, stX++, WALL);
+      mvaddch(stY, stX++, WALL); refresh();
       refresh();
-      napms(40);
+      napms(TINT);
     }
   } else {
     for (int i = 0; i < width + edge; ++i) {
       mvaddch(stY, stX--, WALL);
       refresh();
-      napms(40);
+      napms(TINT);
     }
   } 
 }
 
+/*Prints Vertical Wall*/
 void PrintVertical(int height, int stX, int stY, int edge, bool up) {
   if(up) {
     for (int i = 0; i < height + edge; ++i) {
       mvaddch(stY--, stX, WALL);
       refresh();
-      napms(40);
+      napms(TINT);
     }
   } else {
       for (int i = 0; i < height + edge; ++i) {
       mvaddch(stY++, stX, WALL);
       refresh();
-      napms(40);
+      napms(TINT);
    }
   }
 }
 
-void PrintWall(int rows, int columns, char mat[rows][columns], int startX, int startY) {
+/*This Function Prints Presets, see examples below in PrintMap and Read Documentation about AllocatePresets*/
+void PrintWalls(int rows, int columns, char** mat, int startX, int startY) {
     int currentX = startX;
     int currentY = startY;
 
@@ -47,7 +54,7 @@ void PrintWall(int rows, int columns, char mat[rows][columns], int startX, int s
 	    if (mat[i][j] == WALL) {
 		    mvaddch(currentY, currentX + j, WALL);
 		    refresh();
-		    napms(40);
+		    napms(TINT);
 	    }
 	}
 	currentX = startX;
@@ -55,22 +62,56 @@ void PrintWall(int rows, int columns, char mat[rows][columns], int startX, int s
     }
 }
 
+/*If you want to add your own preset, you must Allocate it using function AllocatePresets:
+/-------------------------------------------------------------------------------------------------------
+*size: Argument size represents the number of presets that should be allocated;
+/-------------------------------------------------------------------------------------------------------
+*mat: Argument mat is Matrix where this function allocates current Preset (in our example it is presets);
+/-------------------------------------------------------------------------------------------------------
+*firstD: Argument firstD represents a number of rows that your Preset contains 
+    (in our example this argument remains the same for all presets and equals to 15);
+/------------------------------------------------------------------------------------------------------- 
+*secondD: Argument secondD represents a number of columns that your Preset contains
+    (in our example this argument remains the same for all presets and equals to 30);
+/-------------------------------------------------------------------------------------------------------
+*preset: Argument preset represents current preset that you want to allocate
+/-------------------------------------------------------------------------------------------------------
+*index: Argument index represents a Matrix index that you will use to refer to your preset
+/-------------------------------------------------------------------------------------------------------*/ 
+void AllocatePresets(int size, char** mat[size], int firstD, int secondD, char preset[firstD][secondD], int index) {
+   mat[index] = malloc(firstD * sizeof(char*));
+   for (int i = 0; i < firstD; i++) {
+	mat[index][i] = malloc(secondD * sizeof(char));
+	for (int j = 0; j < secondD; j++) {
+	    mat[index][i][j] = preset[i][j];
+	}    
+   }
+}
+
+/*Free Space after Allocating Matrix*/
+void FreeSpace(int size, int firstD, char** mat[size]) {
+    for (int matIt = 0; matIt < size; matIt++) {
+	for (int i = 0; i < firstD; i++) {
+	    free(mat[matIt][i]);
+	}
+	free(mat[matIt]);
+    }
+}	
+
 void PrintMap(int width, int height) {
+  int edge = 3;
   int x;
-  int y;
+  int y; 
+  
+  /*define the width and the height of squares*/
+  int sqrWidth = width/2;
+  int sqrHeight = height/2;
   
   /*allocate coordinates of the center*/
   getmaxyx(stdscr, y, x);
   int centerX = x/2;
-  int centerY = y/2;
-
-  /* should be removed after testing*/
-  //mvaddch(centerY, centerX, '#');
-
-  /*define the width and the height of squares*/
-  int sqrWidth = width/2;
-  int sqrHeight = height/2;
-
+  int centerY = y/2; 
+  
   /*selecting the starting point of drawing
    * in this case this point is bottom-right node of the map */
   int botRightX = centerX + sqrWidth + 1;
@@ -84,17 +125,39 @@ void PrintMap(int width, int height) {
   int topLeftX = botRightX - width - 2;
   int topLeftY = topRightY;
 
-  /*calculation the bot-left node of the map*/
+  /*calculating the bot-left node of the map*/
   int botLeftX = topLeftX;
   int botLeftY = botRightY; 
+  
   /*Drawing edge walls*/
-  PrintVertical(height, botRightX, botRightY, 3, true);
-  PrintHorizontal(width, topRightX, topRightY, 3, false);
-  PrintVertical(height, topLeftX, topLeftY, 3, false);
-  PrintHorizontal(width, botLeftX, botLeftY, 3, true);
+  PrintVertical(height, botRightX, botRightY, edge, true);
+  PrintHorizontal(width, topRightX, topRightY, edge, false);
+  PrintVertical(height, topLeftX, topLeftY, edge, false);
+  PrintHorizontal(width, botLeftX, botLeftY, edge, true);
 
-  PrintWall(15, 30, presetN1, topLeftX + 1, topLeftY + 1);
-  PrintWall(15, 30, presetN3, centerX + 1, centerY +1);
-  PrintWall(15, 30, presetN4, topLeftX + 1, topLeftY + 1 + 16);
-  PrintWall(15, 30, presetN2, centerX + 1, centerY - 15);
+  /*Allocating Matrix of Default Presets*/    
+  int size = 10; 
+  char** presets[size];
+  
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN1, 0);
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN2, 1);
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN3, 2);
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN4, 3);
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN5, 4);
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN6, 5);
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN7, 6);
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN8, 7);
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN9, 8);
+  AllocatePresets(size, presets, sqrHeight, sqrWidth, presetN10, 9);
+  
+  /*Organizing Random Presets Drawing*/
+  /*(Will be added Soon)*/
+
+  /*Print presets*/
+  PrintWalls(sqrHeight, sqrWidth, presets[4], topLeftX + 1, topLeftY + 1);
+  PrintWalls(sqrHeight, sqrWidth, presets[1], centerX + 1, centerY +1);
+  PrintWalls(sqrHeight, sqrWidth, presets[9], topLeftX + 1, topLeftY + 1 + 16);
+  PrintWalls(sqrHeight, sqrWidth, presets[5], centerX + 1, centerY - 15);
+
+  FreeSpace(size, sqrHeight, presets);
 }
--- rooms.h
+++ rooms.h
@@ -2,9 +2,17 @@
 #define ROOMS_DEMO_H
 #include<stdbool.h>
 
+/*To see what each fuction does, visit rooms.c*/
+
 void PrintVertical(int height, int stX, int stY, int edge, bool up);
+
 void PrintHorizontal(int width, int stX, int stY, int edge, bool right); 
-void PrintWall(int rows, int colunms, char mat[rows][colunms], int startX, int startY);
+
+void PrintWalls(int rows, int colunms, char** mat, int startX, int startY);
+
 void PrintMap();
 
+void AllocatePresets(int size, char** mat[size], int firstD, int secondD, char preset[firstD][secondD], int index);
+
+void FreeSpace(int size, int firstD, char** mat[size]);
 #endif
