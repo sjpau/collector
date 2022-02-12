3dc32bce3f82c19342dc9f5d7946d7a5ac18ea53
flat <flatdogminsk@gmail.com>
2022-02-12 23:55:32 +0300

    window support added

--- main.c
+++ main.c
@@ -4,22 +4,30 @@
 #include "rooms.h"
 #include "mwin.h"
 
-int main(){
+int main(int argc, char *argv[]){
 
+  //WINDOW* gameWindow;
   initscr();
   raw();
-  cbreak();
+  //cbreak();
   noecho(); 
-  curs_set(0);
+  //printw("TEST");
+  refresh();
+  //curs_set(0);
 /*screen initialized*/
 	//	PrintMap(stdscr, 60, 30);
-	mainMenuGetAttrs();
-	refresh();
-	InitMainMenu();
+	//mainMenuGetAttrs();
+	//refresh();
+	//InitMainMenu();
 /*main game loop*/
 //	setCharacterMovementUp();
 	/*screen closed*/
-  refresh();
+  WINDOW* gameWindow = newwin(40, 80, 15, 15); 
+  box(gameWindow, 0, 0); 
+  wrefresh(gameWindow);
+  PrintMap(gameWindow, 60, 30);
+
+  getch();
   endwin();
 
   return 0;
