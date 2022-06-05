98517368115526026a3244089579b4f916ab1830
Mobyus <theonlymoby@protonmail.com>
2022-06-05 13:42:59 +0300

    spring cleaning removed windows

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <ncurses.h>
 #include "mv.h"
-#include "dial.c"
 #include "rooms.h"
-#include "mwin.h"
 #include <stdlib.h>
 
 char *mainMenuChoices[] = {
@@ -38,31 +36,13 @@ int action = 0; /* 1. Start game
 InitCurses();
 /*screen initialized*/
 	
-x = (getmaxx(stdscr) - MM_WIDTH) / 2; 
-y = (getmaxy(stdscr) - MM_HEIGHT) / 2;
-WINDOW *mainMenu = InitWin(y, x, MM_WIDTH, MM_HEIGHT);	
-
-MainMenuAction(mainMenu, mainMenuChoices, &numberOfMainMenuChoices, &highlight, &currentChoice, &action);
-	do{	
 	
 	int Y = GetCenterY(stdscr);
 	int X = GetCenterX(stdscr);
 	
-	switch (action){
-		case 1:
 			PrintMap(stdscr, 60, 30);
 			StartMainGameLoop(stdscr, &Y, &X);
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-	}
 	
-		break;
-	} while (1);
 /*screen closed*/
 
   endwin();
